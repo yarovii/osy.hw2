63c1f1a384a292da4e2f2494384c16f8051bb878
yarovii <56606724+yarovii@users.noreply.github.com>
2020-04-16 00:35:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <cstdint>
 #include <cassert>
 #include <cmath>
+
+#include <time.h>
+#include <vector>
+#include <set>
 using namespace std;
 #endif /* __PROGTEST__ */
 
@@ -25,7 +29,7 @@ class MemManager {
 
      uint8_t * localMemPool;
      uint8_t structSize;
-     uint8_t countDone;
+     size_t countDone=0;
      HeapMemory* tmp;
      static MemManager* instanceVar;
 
@@ -55,10 +59,7 @@ public:
 MemManager* MemManager::instanceVar=0;
 
 void MemManager::Init(void * memPool, int memSize){
-    localMemPool =(uint8_t *) memPool+ sizeof(MemManager);
-
-//    localMemPool[0] = 0;
-//    localMemPool[1] = sizeof(HeapMemory);
+    localMemPool =(uint8_t *) memPool + sizeof(MemManager);
 
     countDone = 0;
     structSize = sizeof(HeapMemory);
@@ -73,7 +74,7 @@ void * MemManager::Alloc ( int size ){
     while(true){
         if((*tmp).size >= size && (*tmp).isFree) {
             ///Creates next free node
-            if((*tmp).size > size) {
+            if(tmp->size - size > structSize && (*tmp).size > size) {
                 tmpOb = HeapMemory(tmp->index + size + structSize, tmp->size - size - structSize, true, tmp, NULL);   ///MAYBE +1
                 memcpy(localMemPool + ( *tmp ).index + size, (uint8_t *) &tmpOb, structSize);                          ///////problem
                 tmp->size = size;
@@ -125,20 +126,20 @@ bool    MemManager::Free    ( void * blk ){
     tmp = (HeapMemory*)blk;
     tmp--;
 
-    if( tmp->isFree || tmp->index == 0)
+    if( tmp->isFree || tmp->index < 32 || tmp->size < 1)
         return false;
 
     tmp->isFree = true;
+    --countDone;
 
     if(tmp->prev != NULL && tmp->next != NULL){
         collectFreeAll((HeapMemory*)tmp->prev, tmp, (HeapMemory*)tmp->next);
     }
-    else if(tmp->prev != NULL)
+    else if(tmp->prev != NULL && tmp->prev->isFree)
         collectFree(tmp, (HeapMemory*)tmp->prev);
     else if(tmp->next != NULL)
         collectFree((HeapMemory*)tmp->next, tmp);
 
-    --countDone;
     return true;
 }
 
@@ -168,45 +169,195 @@ void   HeapDone    ( int  * pendingBlk )
 }
 
 #ifndef __PROGTEST__
+
+void test(void * memPool, int  pendingBlk){
+    srand (time(NULL));
+    int r=0, s=1000, size = sizeof(uint8_t*)*1000;
+
+//    uint8_t ** arr = (uint8_t**) malloc(size);
+    vector<uint8_t* > arr ;
+
+    vector<int> output;
+    int ss=1048576;
+    int demoss=0;
+
+    HeapInit ( memPool, ss );
+
+    int a=0, f=0, o=0, er=0;
+
+    for(int i =0; i < s; i++){
+        r = rand() % 3;
+        switch(r){
+            case 0:
+                if(size>0) {
+                    r = rand() % ss;
+                    uint8_t *hm = (uint8_t *) HeapAlloc(r);
+//                    arr[a] = (uint8_t *) HeapAlloc(r);
+                    if ( hm == NULL );
+//                        printf("NULL ALLOC    %i\n", r);
+//                        o=0;
+                    else {
+                        printf("ALLOC    %i      %i\n", r, a);
+                        arr.push_back(hm);
+//                        if ( arr[a] != NULL ){
+                        a++;
+                        output.push_back(r);
+                        ++o;
+                        demoss += r;
+                        if(demoss > ss)
+                            printf("OPA TUT NE NADO TAK\n");
+                    }
+                }
+                break;
+            case 1:
+                if(a > 0) {
+                    f = rand() % a;
+                    uint8_t * hm = arr.at(f);
+                    if(HeapFree(hm)){
+                        demoss -= output.at(f);
+                        output.at(f) = 0;
+                        printf("freee    %i\n", f);
+                        --o;
+                    }
+                }
+                break;
+            case 2:
+                HeapDone ( &pendingBlk );
+                if(pendingBlk != o)
+                    printf("PROBLEM   %i    %i\n", pendingBlk, o, er++);
+                break;
+
+            default: break;
+
+        }
+    }
+    int all=0;
+    for(int i=0; i < output.size(); i ++){
+        if(all != all+ output.at(i)) {
+            all += output.at(i);
+            printf("%i   %i    %i\n", output.at(i), all, i);
+        }
+    }
+    if(all >= ss)
+        printf("TREVOGAAAA \n");
+    HeapDone ( &pendingBlk );
+        printf("SSSS %i      %i      %i   %i      %i\n", all, ss, ss-all, er, pendingBlk);
+}
+
 int main ( void )
 {
     uint8_t       * p0, *p1, *p2, *p3, *p4;
     int             pendingBlk;
     static uint8_t  memPool[3 * 1048576];
-//    uint8_t * memPool = (uint8_t *) calloc (2097152, sizeof(uint8_t));
-//    static uint8_t  memPool[3 * 1048576];
 
-    /*HeapInit ( memPool, 0 );
-    p0 = (uint8_t*) HeapAlloc ( (2097152/2)-32 );
-    memset ( p0, 1, (2097152/2)-32 );
-    p1 = (uint8_t*) HeapAlloc ( (2097152/2)-32 );
-    memset ( p1, 1, (2097152/2)-32 );
-//    printf("%i %i", p0, p1);
+
+    /*HeapInit ( memPool, 1048576 );
+    p0 = (uint8_t*) HeapAlloc ( 602059 );
+    printf("mmm   %i\n", p0);
+    HeapDone ( &pendingBlk );
+    printf("done   %i \n", pendingBlk);
+    p0 = (uint8_t*) HeapAlloc ( 446000 );
+    printf("mmm   %i\n", p0);
+    HeapDone ( &pendingBlk );
+    printf("done   %i \n", pendingBlk);
+
+    HeapFree ( p0 );
+    HeapDone ( &pendingBlk );
+    printf("done   %i \n", pendingBlk);
+//    memset ( p1, 10, 10 );
+    p2 = (uint8_t*) HeapAlloc ( 1048576-32-64 );
+    printf("mmm   %i     hhh 3\n", p2);
+    HeapDone ( &pendingBlk );
+    printf("done   %i \n", pendingBlk);*/
+//    memset ( p2, 11, 11 );
+
+    /*HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 3\n", pendingBlk);
+    p2 = (uint8_t*) HeapAlloc ( 10 );
+    printf("mmm   %i    p222\n", p2);
+    p3 = (uint8_t*) HeapAlloc ( 11 );
+    printf("mmm   %i    p333\n", p3);
+
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 3\n", pendingBlk);*/
+
+    /*p3 = (uint8_t*) HeapAlloc ( 39 );
+    memset ( p3, 39, 39 );
+    HeapFree ( p3 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 3\n", pendingBlk);
+    p3 = (uint8_t*) HeapAlloc ( 40 );
+    memset ( p3, 4, 40 );
+    HeapFree ( p0 );
+    HeapFree ( p2 );
+    HeapFree ( p1 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 1\n", pendingBlk);
+
+    p0 = (uint8_t*) HeapAlloc ( 5 );
+    memset ( p0, 5, 5 );
+//    HeapFree ( p0 );
+    p1 = (uint8_t*) HeapAlloc ( 10 );
+    memset ( p1, 10, 10 );
+    p2 = (uint8_t*) HeapAlloc ( 11 );
+    memset ( p2, 11, 11 );
+//    HeapFree ( p0 );
+    HeapFree ( p2 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 3\n", pendingBlk);
+    p2 = (uint8_t*) HeapAlloc ( 11 );
+    memset ( p2, 11, 11 );
+    HeapFree ( p0 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 3\n", pendingBlk);
+    p0 = (uint8_t*) HeapAlloc ( 5 );
+    memset ( p0, 5, 5 );
+    HeapFree ( p0 );
+    HeapFree ( p2 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 2\n", pendingBlk);
+    p2 = (uint8_t*) HeapAlloc ( 11 );
+    memset ( p2, 11, 11 );
+    HeapFree ( p0 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 2\n", pendingBlk);
+    p0 = (uint8_t*) HeapAlloc ( 5 );
+    memset ( p0, 5, 5 );
+    HeapFree ( p1 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 3\n", pendingBlk);
+    p1 = (uint8_t*) HeapAlloc ( 10 );
+    memset ( p1, 10, 10 );
     HeapFree ( p0 );
     HeapFree ( p1 );
-    p0 = (uint8_t*) HeapAlloc ( 2097152-32 );
-    memset ( p0, 1, 2097152-32 );
-    HeapFree ( p0 );
-    p0 = (uint8_t*) HeapAlloc ( 1 );
-    memset ( p0, 1, 1 );
-    HeapFree ( p0 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 2\n", pendingBlk);
+    p1 = (uint8_t*) HeapAlloc ( 10 );
+    memset ( p1, 10, 10 );
+    p0 = (uint8_t*) HeapAlloc ( 5 );
+    memset ( p0, 5, 5 );
+    HeapDone ( &pendingBlk );
+    printf("mmm   %i     hhh 4\n", pendingBlk);*/
+
+//    for(int i=0; i < 251; i++)
+//        printf("mmm   %i     %i\n", i, memPool[i]);
 
 
-    HeapInit ( memPool, 1000);
+    /*HeapInit ( memPool, 1000);
     p0 = (uint8_t*) HeapAlloc ( 100 );
 //    printf("%i %i", p0, p0);
     memset ( p0, 1, 100 );
     p1 = (uint8_t*) HeapAlloc ( 250 );
     memset ( p1, 1, 250 );
     p2 = (uint8_t*) HeapAlloc ( 450 );
-    memset ( p2, 1, 451 );
+    memset ( p2, 1, 450 );
     p3 = (uint8_t*) HeapAlloc ( 71 );
-    memset ( p3, 1, 71 );
+//    memset ( p3, 1, 71 );*/
 
 //    for(int i=0; i < 1020; i++)
 //        printf("mm   %i    %i\n", i, memPool[i]);
 //    printf("%i %i", p0, p3);
-    HeapFree ( p1 );
+    /*HeapFree ( p1 );
     HeapFree ( p2 );
     p2 = (uint8_t*) HeapAlloc ( 732 );
     memset ( p2, 0, 732 );
@@ -214,9 +365,8 @@ int main ( void )
     HeapFree ( p0 );
     p0 = (uint8_t*) HeapAlloc ( 1 );
     memset ( p0, 0, 2 );
-    HeapFree ( p0 );
+    HeapFree ( p0 );*/
 
-*/
 
     HeapInit ( memPool, 2097152 );
     assert ( ( p0 = (uint8_t*) HeapAlloc ( 512000 ) ) != NULL );
@@ -280,5 +430,6 @@ int main ( void )
 
     return 0;
 }
+
 #endif /* __PROGTEST__ */
 
